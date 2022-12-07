ba3d486584763f92b57680d6f0360836691ccb97
vladimir golovin <tkomplect@mail.ru>
2022-12-08 01:32:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+//  Задача 10 Показать вторую цифру трехзначного числа.
+
+
+//Console.Write("Введите трехзначное значение: ");
+//int i = int.Parse(Console.ReadLine()!);
+//string array = i.ToString(); // Строковое представление , например:918
+//if (i > 0)
+//    Console.WriteLine(array[1]);  // Вывод в консоль значение индекса 1 : это у строки 918 будет 1
+//if (i < 0)
+//    Console.WriteLine(array[2]);  // Если значение отрицательное, например -918
+
+
+
+
+
+
+//  Задача 13 Вывод третьей цифры или вывести её отсутствие.
+
+//Console.Write("Введите значение: ");
+//int i = int.Parse(Console.ReadLine()!);
+//if (i <= 99)
+//{
+//    Console.WriteLine("Третьей цифры нет!");
+//}
+//else
+//{
+//    string array = i.ToString(); // Строковое представление, например: 645
+//    Console.WriteLine(array[2]);  // Вывод в консоль значение индекса 2 : это у 645 будет 5
+//}
+
+
+
+
+
+
+
+
+
+// Задача 15 Вывод дней недели при вводе цифры дня недели и вывод выходного дня, если цифра соответствует.
+
+//int number = int.Parse(Console.ReadLine());
+
+//if (number == 6 || number == 7)
+//{
+//    Console.WriteLine("Выходной");
+//}
+//switch (number)
+//{
+//    case 1:
+//        Console.WriteLine("Понедельник");
+//        break;
+//    case 2:
+//        Console.WriteLine("Вторник");
+//        break;
+//    case 3:
+//        Console.WriteLine("Среда");
+//        break;
+//    case 4:
+//        Console.WriteLine("Четверг");
+//        break;
+//    case 5:
+//        Console.WriteLine("Пятница");
+//        break;
+//    case 6:
+//        Console.WriteLine("Суббота");
+//        break;
+//    case 7:
+//        Console.WriteLine("Воскресенье");
+//        break;
+//    default:
+//        break;
+//}
+
+
+
+
+
